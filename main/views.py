4485197aa8545db68b57962f22c6d5c261d962bd
Sewer01 <sewer910@gmail.com>
2021-09-12 11:16:31 +0300

    about

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(reguest):
     return HttpResponse("<h4> Hello Sergey!!! </h4>")
+
+def about(reguest):
+    return HttpResponse("<h4> My name Sergey!!! </h4>")
