cac1c3ef8c8930bf1381975cbaa62b8809c2687c
Sewer01 <sewer910@gmail.com>
2021-09-14 00:41:35 +0300

    mod

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models.fields import CharField
 
-class Task(models.Model)
+class Task(models.Model):
     title = models.CharField('NAME', max_length=50)
     task = models.TextField('CONTENT')
 
