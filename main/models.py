10c782834b127e982a176a73b374454dc1b32f89
Sewer01 <sewer910@gmail.com>
2021-09-14 00:39:00 +0300

    title

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+class Task(models.Model)
+    title = models.CharField('NAME', max_length=50)
+    task = models.TextField('CONTENT')
+
+    def __str__(self):
+        return self.title
