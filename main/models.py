245bac6f23369143ee1f17e6cb0e3c2acbe016ff
Sewer01 <sewer910@gmail.com>
2021-09-14 13:40:11 +0300

    Meta

--- main/models.py
+++ main/models.py
@@ -8,6 +8,6 @@ class Task(models.Model):
     def __str__(self):
         return self.title
 
-    class meta:
+    class Meta:
         verbos_name = 'Задание'
         verbos_name_plural = 'Задания'
