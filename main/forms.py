e397b4b98349da1c2d34fd253e7a482da0068369
Sewer01 <sewer910@gmail.com>
2021-09-14 15:53:18 +0300

    x3

--- main/forms.py
+++ main/forms.py
@@ -5,13 +5,13 @@ from django.forms import ModelForm, widgets, TextInput
 class TaskForm(ModelForm):
     class Meta:
         model = Task
-        fields = ['title','task']
+        fields = ["title","task"]
         widgets = {
-            'title':TextInput(attrs={
+            "title":TextInput(attrs={
                 'class':'form-control',
-                'placeholder':"Введите название"}),
-            'task':TextInput(attrs={
+                'placeholder':'Введите название'}),
+            "task": TextInput(attrs={
                 'class':'form-control',
-                'placeholder':"Введите описание"})   
+                'placeholder':'Введите описание'}),   
                 
         }
